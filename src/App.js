4aac6820c69a18290c0781f1b06aa7d4a8a16fe0
Phamty2002 <114122026+Phamty2002@users.noreply.github.com>
2023-12-19 18:19:29 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Ex1 from './components/ex1/Ex1';
+import Ex11 from './components/Ex1.1/Ex1.1';
+import Ex12 from './components/Ex1.2/Ex12';
+import Ex13 from './components/Ex1.3/Navigation';
+import Ex131 from './components/Ex1.3.1/Navigation-2';
+import Ex132 from './components/Ex1.3.2/App';
+import Ex14 from './components/Ex1.4/Ex1.4';
+import Ex15 from './components/Ex1.5/Ex1.5';
+import Ex16 from './components/Ex1.6/Ex1.6';
+import Ex17 from './components/Ex1.7/App';
+import Ex171 from './components/Ex1.7.1/scripts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ex1 />
+      <hr />
+      <Ex11 />
+      <hr />
+      <Ex12 />
+      <hr />
+      <Ex13 />
+      <hr />
+      <Ex131 />
+      <hr />
+      <Ex132 />
+      <hr />
+      <Ex14 />
+      <hr />
+      <Ex15 />
+      <hr />
+      <Ex16 />
+      <hr />
+      <Ex17 />
     </div>
   );
 }
